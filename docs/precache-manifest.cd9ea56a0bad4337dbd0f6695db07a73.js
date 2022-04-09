358a667a7b2e644e6b83f1c430653269813db6b0
JonathanBorbor <jonathanbor2000@hotmail.com>
2022-04-09 09:28:27 -0500

    docs add

---
+++ docs/precache-manifest.cd9ea56a0bad4337dbd0f6695db07a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef74097676eba3eb2d013b289057630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29d299700ce38c2b8087",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d299700ce38c2b8087",
+    "url": "/static/js/main.0cfa6685.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
